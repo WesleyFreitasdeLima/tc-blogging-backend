85ea33c6555a21e3e7587f25cc383d48a7a96c84
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2026-05-20 22:59:20 -0300

    feat: implement PostService for retrieving blog posts

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,13 +1,11 @@
 import type { Request, Response } from "express";
 import type { Post } from "../models/Post.js";
+import PostService from "../services/PostService.js";
 
 class PostController {
   static getAllPosts(req: Request, res: Response): Response {
     try {
-      const posts: Post[] = [
-            { id: 1, title: "Primeiros passos com Express", content: "Express é uma ferramenta incrível...", author: "Dev" },
-            { id: 2, title: "Dominando o MVC", content: "Arquitetura limpa facilita muito a manutenção...", author: "Dev" }
-        ];
+      const posts: Post[] = PostService.getAllPosts();
 
       return res.status(200).json({ 
         message: "All posts retrieved successfully",
